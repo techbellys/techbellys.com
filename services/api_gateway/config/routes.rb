1835338a6e6a16a054c27d962ffe791b9e1b1945
prabhat pankaj <prabhatiitbhu@gmail.com>
2025-06-09 16:22:36 +0530

    api_gateway

--- services/api_gateway/config/routes.rb
+++ services/api_gateway/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       match 'auth/*path', to: 'proxy#forward_auth', via: :all
-      match 'jobs/*path', to: 'proxy#forward_jobs', via: :all
     end
   end
 end
--- services/api_gateway/test/controllers/api/v1/proxy_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Api::V1::ProxyControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
